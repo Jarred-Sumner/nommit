cbcaa2a53362cd33e90436072e17e6b4fd0f76e1
loopyz <lguo@andrew.cmu.edu>
2014-08-30 16:28:45 -0400

    adding random images in

---
+++ nommit/Controllers/NMSalesViewController.h
@@ -0,0 +1,13 @@
+//
+//  NMSalesViewController.h
+//  nommit
+//
+//  Created by Lucy Guo on 8/30/14.
+//  Copyright (c) 2014 Lucy Guo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NMSalesViewController : UIViewController
+
+@end
---
+++ nommit/Custom Layout/NMCollectionViewLargeLayout.h
@@ -0,0 +1,13 @@
+//
+//  NMCollectionViewLargeLayout.h
+//  nommit
+//
+//  Created by Lucy Guo on 8/30/14.
+//  Copyright (c) 2014 Lucy Guo. All rights reserved.
+//
+
+@import UIKit;
+
+@interface NMCollectionViewLargeLayout : UICollectionViewFlowLayout
+
+@end
