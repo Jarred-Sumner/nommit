9a80f8824f80b22a6b35ce43a1a7288cdef30104
loopyz <lguo@andrew.cmu.edu>
2014-09-01 18:20:05 -0400

    progress bar

--- nommit/Views/NMCampaignInfoView.m
+++ nommit/Views/NMCampaignInfoView.m
@@ -7,6 +7,7 @@
 //
 
 #import "NMCampaignInfoView.h"
+#import <TYMProgressBarView.h>
 
 @implementation NMCampaignInfoView
 
@@ -18,8 +19,27 @@
         self.backgroundColor = [UIColor whiteColor];
         self.layer.borderColor = [UIColorFromRGB(0xD3D3D3) CGColor];
         self.layer.borderWidth = 1.0f;
+        
+        [self setupProgressBar:numberLeft withnumberTotal:numberTotal];
     }
     return self;
+}
+
+- (void)setupProgressBar:(NSUInteger)numberLeft withnumberTotal:(NSUInteger)numberTotal
+{
+    // Create a progress bar view and set its appearance
+    TYMProgressBarView *progressBarView = [[TYMProgressBarView alloc] initWithFrame:CGRectMake(15, 30, self.frame.size.width - 30, 15)];
+    progressBarView.barBorderWidth = 1.0f;
+    progressBarView.barBorderWidth = 0;
+    progressBarView.barBackgroundColor = UIColorFromRGB(0xDCDCDC);
+    progressBarView.barFillColor = UIColorFromRGB(0x42B7BB);
+    progressBarView.barInnerPadding = 0;
+    
+    // Add it to your view
+    [self addSubview:progressBarView];
+    
+    // Set the progress
+    progressBarView.progress = ([[NSNumber numberWithInt:numberLeft] floatValue]/[[NSNumber numberWithInt:numberTotal] floatValue]);
 }
 
 /*
