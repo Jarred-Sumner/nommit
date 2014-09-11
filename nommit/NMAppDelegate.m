1badc50867d75a079f29721a83642f5277b030b7
Jarred Sumner <jarred@jarredsumner.com>
2014-09-11 13:50:26 -0700

    Don't fire off a request on app launch

--- nommit/NMAppDelegate.m
+++ nommit/NMAppDelegate.m
@@ -33,19 +33,6 @@
     
     // UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:[[NMFoodsViewController alloc] init]];
     
-    NMApi *api = [[NMApi alloc] initWithBaseURL:[NSURL URLWithString:@"http://localhost:3000"] managedObjectContext:[NSManagedObjectContext MR_defaultContext] sessionConfiguration:nil];
-    [api GET:@"orders" parameters:@{@"access_token": @"CAACEdEose0cBAJprq53f0eXCDewFj5Pq7FmZB0mw8Ov1ILns1AETYxc2fSMvApwujSawDnI2PbXjxVljHt1YDcHogBp0yzv3YNSK2DxM7ikVgNz3YfMD9lOZCEyOxA5HILiZClsuadjjUwZCcrcowrzejvLOFZCZBfMuoM8p2TrVcodV6ajFxJpLvl6mcj0XIhcEFKiXsn5rTIqpZBhCzdZBxBJGnZAA54wIZD"} completion:^(OVCResponse *response, NSError *error) {
-        NSArray *models = [MTLJSONAdapter modelsOfClass:[NMOrderApiModel class] fromJSONArray:response.result error:&error];
-        NSLog(@"Models: %@", error);
-        NMOrder *order = [MTLManagedObjectAdapter managedObjectFromModel:models[0] insertingIntoContext:[NSManagedObjectContext MR_defaultContext] error:&error];
-        NSLog(@"Order: %@", error);
-        NSLog(@"Order Count: %d", [NMOrder MR_countOfEntities]);
-        NSLog(@"Food Count: %d", [NMFood MR_countOfEntities]);
-        NSLog(@"Address Count: %d", [NMAddress MR_countOfEntities]);
-        NSLog(@"User Count: %d", [NMUser MR_countOfEntities]);
-    }];
-
-    
     // create content and menu controllers
     NMMenuNavigationController *navigationController = [[NMMenuNavigationController alloc] initWithRootViewController:[[NMLoginViewController alloc] init]];
     NMMenuViewController *menuController = [[NMMenuViewController alloc] initWithStyle:UITableViewStylePlain];
